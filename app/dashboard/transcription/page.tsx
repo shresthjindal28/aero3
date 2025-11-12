236f8c3b37e32c4e60c5e13c27d80bc165e4d608
shresthjindal28 <shresthjindal28@gmail.com>
2025-11-12 16:37:02 +0530

    new push from new branch

--- app/dashboard/transcription/page.tsx
+++ app/dashboard/transcription/page.tsx
@@ -1,13 +1,23 @@
-import Transcription from '@/components/transcription'
-import React from 'react'
+import Transcription from "@/components/transcription";
+import React, { useEffect } from "react";
+import { socket } from "@/lib/socket";
 
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+    return () => {
+      socket.off("connect");
+      socket.disconnect();
+    };
+  }, []);
   return (
-    <div className='min-w-[80vw] mx-auto p-4 md:p-8 flex items-center flex-col justify-between'>
-      <Transcription />
-      
+    <div className="min-w-[80vw] mx-auto p-4 md:p-8 flex items-center flex-col justify-between">
+      <Transcription socket={socket} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,7 @@
 "use client";
 
-import Transcription from "@/components/transcription";
-import { socket } from "@/lib/socket";
-import React, { useEffect } from "react";
-
 const page = () => {
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected");
-    });
-    return () => {
-      socket.off("connect");
-      socket.disconnect();
-    };
-  }, []);
-  return (
-    <div>
-      <Transcription socket={socket} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
